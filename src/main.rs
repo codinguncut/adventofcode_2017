5501cb5692f1f4f02cad33cc828333f821c9a708
Johannes Ahlmann <johannes@fluquid.com>
2017-12-04 13:25:29 +0000

    day 3,1

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,17 @@ fn read_file(fname: &str) -> String {
 }
 
 
-#[allow(dead_code)]
 fn advent1() {
     // get characters that are the same in 's' with distance 'offset'
     fn get_same(s: String, offset: usize) -> Vec<char> {
         let t = [&s[offset..], &s[..offset]].join("");
-        let x = s.chars().zip(t.chars())
+        return s.chars().zip(t.chars())
             .filter(|&(a, b)| a == b)
             .map(|(a, _)| a)
             .collect::<Vec<_>>();
-        return x;
     }
 
     // turn vector of characters into vector of numbers
-    // Note: no error handling
     fn to_num(a: Vec<char>) -> Vec<u32> {
         return a
             .iter()
@@ -39,21 +36,40 @@ fn advent1() {
             .collect::<Vec<_>>();
     }
 
+    fn worker(s: String, offset: usize) -> u32 {
+        return to_num(get_same(s, offset)).iter().sum();
+    }
+
+    for &(sample, valid) in [
+        ("1122", 3),
+        ("1111", 4),
+        ("1234", 0),
+        ("91212129", 9)
+    ].iter() {
+        assert_eq!(worker(sample.to_string(), 1), valid);
+    }
+
+    for &(sample, valid) in [
+        ("1212", 6),
+        ("1221", 0),
+        ("123425", 4),
+        ("123123", 12),
+        ("12131415", 4),
+    ].iter() {
+        let offset = sample.chars().count() / 2 as usize;
+        assert_eq!(worker(sample.to_string(), offset), valid);
+    }
+
     let input = read_file("data/day1.txt");
     let offset = input.chars().count() / 2 as usize;
-    let res : u32 = to_num(get_same(input.to_string(), offset))
-        .iter()
-        .sum();
-    println!("{}", res);
+    println!("day 1 - 1 {}", worker(input.to_string(), 1));
+    println!("day 1 - 2 {}", worker(input.to_string(), offset));
 }
 
 
-#[allow(dead_code)]
 fn advent2() {
-    let input = read_file("data/day2.txt");
-
-    fn part1(input: String) {
-        let sum : u32 = input.split('\n').map(|line| {
+    fn part1(input: String) -> u32 {
+        return input.split('\n').map(|line| {
             let (min, max) : (u32, u32) = line
                 .split_whitespace()
                 .map(|s| s.parse().unwrap())
@@ -66,58 +82,94 @@ fn advent2() {
                 .unwrap();
             return max - min;
         }).sum();
-        println!("{}", sum);
     }
-    part1(input.to_string());
 
-    /*
-    let sum : u32 = input.split('\n').map(|line| {
-        let div : u32 = line
-            .split_whitespace()
-            .map(|s| s.parse().unwrap())
-			.tuple_combinations()
-			.filter(|(a, b)| a % b == 0)
-			.map(|(a, b)| a / b as u32)
-			.next()
-			.unwrap();
-        return div;
-    }).sum();
-    println!("{}", sum);
-    */
+    fn part2(input: String) -> u32 {
+        return input.split('\n').map(|line| {
+            let nums = line
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect::<Vec<u32>>();
+            // NOTE: assuming only one match per line
+            let res : u32 = nums.iter().tuple_combinations().map(|(&a, &b)| {
+                if a % b == 0 {
+                    a / b as u32
+                } else if b % a == 0 {
+                    b / a as u32
+                } else {
+                    0
+                }
+            }).sum();
+            return res;
+        }).sum();
+    }
+
+    let sample = "5 1 9 5
+7 5 3
+2 4 6 8".to_string();
+    assert_eq!(part1(sample), 18);
+
+    let sample2 = "5 9 2 8
+9 4 7 3
+3 8 6 5".to_string();
+    assert_eq!(part2(sample2), 9);
+
+    let input = read_file("data/day2.txt");
+    println!("day 2 - 1 {}", part1(input.to_string()));
+    println!("day 2 - 2 {}", part2(input.to_string()));
 }
 
-#[allow(dead_code)]
-fn squares(input: u32) {
-	/*
-	shells: 1^2, 3^2, 5^2, 7^2, ...
-	we can find bottom right corners, and we know length of legs
-	*/
-	let shells = (1..)
-        .filter(|n| n % 2 == 1)
-        .map(|n| n*n)
-        .take_while(|&n| n < input)
-        .collect::<Vec<u32>>();
-	println!("{:?}", shells);
-}
-
-#[allow(dead_code)]
 fn advent3() {
-	//let input : u32 = 361527;
+    fn dist(input: u32) -> u32 {
+        /*
+        shells: 1^2, 3^2, 5^2, 7^2, ...
+        we can find bottom right corners, and we know length of legs
+        total manhatten distance is number of shells + perpendicular offset
+        */
+        if input == 1 {
+            return 0;
+        }
+        let shells = (1..)
+            .filter(|n| n % 2 == 1)
+            .map(|n| n*n)
+            .take_while(|&n| n < input)
+            .collect::<Vec<u32>>();
+        let last_corner = shells.last().cloned().unwrap();
+        let num_shells = shells.len() as i32;
+        let leg_length = (last_corner as f64).sqrt() as u32 + 2;
+        let leg_middle = (((leg_length - 1) / 2) as i32).abs();
+
+        let rest = input - last_corner;
+        let pos_on_leg = (rest % (leg_length - 1)) as i32; 
+        let leg_offset = (pos_on_leg - leg_middle).abs();
+        return (num_shells + leg_offset) as u32;
+    }
+
+    for &(target, steps) in [
+        (1, 0),
+        (12, 3),
+        (23, 2),
+        (21, 4),
+        (1024, 31)
+    ].iter() {
+        assert_eq!(dist(target), steps);
+    }
+
+    println!("day 3 - 1 {}", dist(361527));
 }
 
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
-fn reverse_string(s : String) -> String {
-    return s
-        .chars()
-        .sorted()
-        .iter()
-        .collect::<String>();
-}
-
-#[allow(dead_code)]
 fn advent4() {
+    fn sort_string(s : String) -> String {
+        return s
+            .chars()
+            .sorted()
+            .iter()
+            .collect::<String>();
+    }
+
     fn is_valid(password: String, is_part1 : bool) -> bool {
         let words = password 
             .trim()
@@ -125,27 +177,19 @@ fn advent4() {
             .map(|x| x.to_string())
             .collect::<Vec<String>>();
         let words_len = words.clone().len();
-        if is_part1 {
-            let unique_words : HashSet<String> = HashSet::from_iter(
-                words.iter().cloned()
-            );
-            return words_len == unique_words.len();
-        } else {
-            let unique_words : HashSet<String> = HashSet::from_iter(
-                words
-                .iter()
-                .cloned()
-                .map(reverse_string)
-            );
-            return words_len == unique_words.len();
-        }
+        let unique_words : HashSet<String> = HashSet::from_iter(
+            words
+            .iter()
+            .cloned()
+            .map(|x| if is_part1 {x} else {sort_string(x)})
+        );
+        return words_len == unique_words.len();
     }
 
     fn worker(passwords: String, is_part1: bool) -> usize {
-        let count : usize = passwords.split('\n').map(|line| {
+        return passwords.split('\n').map(|line| {
             if is_valid(line.to_string(), is_part1) {1} else {0}
         }).sum();
-        return count;
     }
 
     for &(sample, valid) in [
@@ -167,13 +211,13 @@ fn advent4() {
     }
 
     let input = read_file("data/day4.txt");
-    println!("{}", worker(input.clone(), true));
-    println!("{}", worker(input, false));
+    println!("day 4 - 1 {}", worker(input.clone(), true));
+    println!("day 4 - 2 {}", worker(input, false));
 }
 
 fn main() {
-    //advent1();
-    //advent2();
-	//squares(1024);
+    advent1();
+    advent2();
+	advent3();
     advent4();
 }
